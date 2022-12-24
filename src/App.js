c6c720e98acb0f72d6ba3d034c657540f64792a4
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2022-12-24 19:17:21 -0300

    feat: interview chakkenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styles from "./App.module.css";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [list, setList] = useState([]);
+  const [newList, setNewList] = useState([]);
+
+  const handleClick = (event) => {
+    const { clientX, clientY } = event;
+    setList((prev) => [...prev, { x: clientX, y: clientY }]);
+  };
+
+  const handleUndo = () => {
+    //event.stopPropagation();
+    if (list.length === 0) return;
+    setNewList((prev) => [...prev, list[list.length - 1]]);
+    setList((prev) => prev.slice(0, prev.length - 1));
+  };
+
+  const handleRendo = () => {
+    //event.stopPropagation();
+    if (newList.length === 0) return;
+    setList((prev) => [...prev, newList[newList.length - 1]]);
+    setNewList((prev) => prev.slice(0, prev.length - 1));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={styles.buttons}>
+        <button onClick={handleUndo} className={styles.button}>
+          Undo
+        </button>
+        <button onClick={handleRendo} className={styles.button}>
+          Rendo
+        </button>
+      </div>
+      <div onClick={handleClick} className={styles.container}>
+        {list.map((item, i) => {
+          return (
+            <span
+              className={styles.circle}
+              key={i}
+              style={{ left: item.x, top: item.y }}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
-
-export default App;
